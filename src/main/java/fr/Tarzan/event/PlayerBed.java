ecbdaa4617c2cd2e505d7c97c433eb8b58f6cb90
MrRazamataz <mrrazamataz@mrrazamataz.ga>
2021-01-04 17:02:03 +0000

    Fix 2.0 (but there is no delay for sleeping)

--- src/main/java/fr/Tarzan/event/PlayerBed.java
+++ src/main/java/fr/Tarzan/event/PlayerBed.java
@@ -1,28 +1,30 @@
 package fr.Tarzan.event;
 
 import cn.nukkit.Player;
+import cn.nukkit.Server;
 import cn.nukkit.event.*;
 import cn.nukkit.event.EventHandler;
 import cn.nukkit.event.Listener;
-import cn.nukkit.event.player.PlayerBedLeaveEvent;
+import cn.nukkit.event.player.PlayerBedEnterEvent;
 import cn.nukkit.level.Level;
 import cn.nukkit.event.player.*;
 import cn.nukkit.network.protocol.*;
 import java.util.*;
 public class PlayerBed implements Listener {
     @EventHandler
-    public void onBed(PlayerBedLeaveEvent event){
+    public void onBed(PlayerBedEnterEvent event){
         Player player = event.getPlayer();
         Level level = event.getBed().getLevel();
-        float oldHealth = player.getHealth();
-        player.attack(1);//.getHandle().a(true,DamageSource.CACTUS);
-        player.setHealth(oldHealth);
-        level.setTime(Level.TIME_DAY);
         level.setRaining(false);
         level.setThundering(false);
         player.setSpawn(player.getLocation());
+        event.setCancelled();
         player.sendMessage("§aRespawn point set!");
         player.getServer().broadcastMessage("[§6OnePlayerSleep§r]§6 It's §6Daytime!");
+        level.setTime(Level.TIME_DAY);
 
+    }
+
+    {
     }
 }
